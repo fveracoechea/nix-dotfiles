ae4ed12ff6e68fe3f621b1c267b56cb5af20c539
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-10-26 13:32:37 -0400

    Update zsh promt styles

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -31,7 +31,7 @@
 
     shellAliases = {
       ls = "eza";
-      la = "eza -abghHli";
+      la = "eza -abghHl --no-user";
       e = "exit";
       c = "clear";
     };
@@ -64,7 +64,7 @@
       ''
         # Completion styling
         zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-        zstyle ':completion:*' list-colors "''${s.:. LS_COLORS}"
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
         zstyle ':completion:*' menu no
         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
         zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
