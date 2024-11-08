4371d8b4e28587256a7377006d5ce8d9c803d92d
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-07 19:16:57 -0500

    Improve pro-audio settings

--- modules/home-manager/pro-audio.nix
+++ modules/home-manager/pro-audio.nix
@@ -1,56 +1,77 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
-  profile = "~/.nix-profile/";
   home = config.home.homeDirectory;
-  lv2Path = "${profile}/lib/lv2";
-  vstPath = "${profile}/lib/vst:${home}/.vst:${home}/.vst/yabridge";
-  vst3Path = "${profile}/lib/vst3";
-  ladspaPath = "${profile}/lib/ladspa";
-in {
-  # Make audio software detect LV2, VST2 and VST3 plugins
-  systemd.user.sessionVariables = {
-    LV2_PATH = lv2Path;
-    VST_PATH = vstPath;
-    LXVST_PATH = vstPath;
-    VST3_PATH = vst3Path;
-    LADSPA_PATH = ladspaPath;
-  };
 
-  home.packages = with pkgs; [
-    audacity
-    tuxguitar
-
-    klick
-    qpwgraph
-
-    # Audio plugins (LV2, VST2, VST3, LADSPA)
-    distrho
-    calf
-    eq10q
+  plugins = with pkgs; [
+    # packs
+    gxplugins-lv2
     lsp-plugins
-    x42-plugins
-    x42-gmsynth
-    dragonfly-reverb
-    guitarix
-    FIL-plugins
+    tap-plugins
+    infamousPlugins
+    # general
+    helm
+    cardinal
+    # Drums
     geonkick
-
-    # Support for Windows VST2/VST3 plugins
-    yabridge
-    yabridgectl
-    wineWowPackages.stable
+    # distortion
+    fire
+    wolf-shaper
   ];
 
-  # Setup Yabridge
-  home.file = {
-    ".config/yabridgectl/config.toml".text = ''
-      plugin_dirs = ['/home/${config.home.username}/.win-vst']
-      vst2_location = 'centralized'
-      no_verify = false
-      blacklist = []
+  # lv2_path will look like:
+  # ${helm}/lib/lv2:$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2
+  # this is just more general to loop over all plugins as makeSearchPath adds lib/lv2 to all
+  # outputs and concat them with a colon in between as explained in:
+  lv2_path = lib.makeSearchPath "lib/lv2" plugins;
+  clap_path = lib.makeSearchPath "lib/clap" plugins;
+  vst3_path = lib.makeSearchPath "lib/vst3" plugins;
+  vst_path = lib.makeSearchPath "lib/vst" plugins;
+  lxvst_path = lib.makeSearchPath "lib/lxvst" plugins;
+  ladspa_path = lib.makeSearchPath "lib/ladspa" plugins;
+  dssi_path = lib.makeSearchPath "lib/dssi" plugins;
+
+  wrapProgram = {
+    programToWrap,
+    filesToWrap ? "*",
+  }:
+    pkgs.runCommand
+    # name of the program, like ardour-with-plugins-8
+    (programToWrap.pname + "-with-plugins-" + programToWrap.version)
+    {
+      nativeBuildInputs = with pkgs; [
+        makeBinaryWrapper
+      ];
+      buildInputs = [
+        programToWrap
+      ];
+    }
+    ''
+      mkdir -p $out/bin
+      for file in ${programToWrap}/bin/${filesToWrap};
+      do
+        filename="$(basename -- $file)"
+        makeWrapper "$file" "$out/bin/$filename"  \
+          --prefix LV2_PATH    : "${lv2_path}"    \
+          --prefix CLAP_PATH   : "${clap_path}"   \
+          --prefix VST3_PATH   : "${vst3_path}"   \
+          --prefix VST_PATH    : "${vst_path}"    \
+          --prefix LXVST_PATH  : "${lxvst_path}"  \
+          --prefix LADSPA_PATH : "${ladspa_path}" \
+          --prefix DSSI_PATH   : "${dssi_path}"   ;
+      done
     '';
-  };
+in {
+  home.packages = with pkgs; [
+    # Apps
+    (wrapProgram {programToWrap = pkgs.ardour;})
+    (wrapProgram {programToWrap = pkgs.audacity;})
+    (wrapProgram {programToWrap = pkgs.zrythm;})
+    (wrapProgram {programToWrap = pkgs.carla;})
+
+    tuxguitar
+  ];
 }
