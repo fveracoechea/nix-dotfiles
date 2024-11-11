ad8ab83a16b047219d3880835dc8d9b6b46c2741
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-10 21:05:05 -0500

    Update darwin config

--- hosts/macbook-pro/configuration.nix
+++ hosts/macbook-pro/configuration.nix
@@ -2,4 +2,10 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 }
