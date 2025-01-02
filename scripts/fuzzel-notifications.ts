e3946ca0b15a549abbe0c388b9d24995188ef1a6
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-01-02 12:35:45 -0500

    update deno script

--- scripts/fuzzel-notifications.ts
+++ scripts/fuzzel-notifications.ts
@@ -1,56 +1,20 @@
-import util from "node:util";
-import { exec as cbExec } from "node:child_process";
+const { stdout, stderr } = await exec('makoctl history');
 
-exec = util.promisify(cdExec);
-
-const icons = {
-  slack: "",
-  teams: "󰊻",
-  outlook: "󰴢",
-  chrome: "",
-  discord: "",
-  vesktop: "",
-  steam: "",
-  blueman: "",
-  bluetooth: "",
-  battery: "󱊣",
-  "input-gaming": "󰊗",
-  default: "󱅫",
-};
-
-function toMenuItem(item) {
-  const appName = item["app-name"].data.trim().toLowerCase();
-  const appIcon = item["app-icon"].data.trim().toLowerCase();
-  const title = item.summary.data.trim();
-  const body = item.body.data.trim();
-
-  const icon =
-    (appIcon && icons[appIcon]) ?? (appName && icons[appName]) ?? icons.default;
-
-  let notification = `${icon}  ${title}`;
-  if (body) notification += `  -  ${body}`;
-  return notification;
+if (stderr) {
+  await exec(`notify-send "Error loading notifications" "${stderr}"`);
+  return;
 }
 
-(async function () {
-  const { stdout, stderr } = await exec("makoctl history");
+const data = JSON.parse(stdout).data[0];
+const inboxIcon = `\\0icon\\x1finbox`;
 
-  if (stderr) {
-    await exec(`notify-send "Error loading notifications" "${stderr}"`);
-    return;
-  }
-
-  const data = JSON.parse(stdout).data[0];
-  const inboxIcon = `\\0icon\\x1finbox`;
-
-  if (!data || data.length < 1) {
-    const fuzzel = `fuzzel -w 50 -p "" --placeholder "Notifications" --dmenu`;
-    await exec(`echo -en "No notifications found.${inboxIcon}" | ${fuzzel}`);
-    return;
-  }
-
-  const notifications = data.map(toMenuItem).join("\n");
-
+if (!data || data.length < 1) {
   const fuzzel = `fuzzel -w 50 -p "" --placeholder "Notifications" --dmenu`;
-  await exec(`echo -en "${notifications}" | ${fuzzel}`);
-})();
+  await exec(`echo -en "No notifications found.${inboxIcon}" | ${fuzzel}`);
+  return;
+}
+
+const notifications = data.map(toMenuItem).join('\n');
+
+const fuzzel = `fuzzel -w 50 -p "" --placeholder "Notifications" --dmenu`;
+await exec(`echo -en "${notifications}" | ${fuzzel}`);
--- scripts/shared/notification.ts
+++ scripts/shared/notification.ts
@@ -1,26 +1,37 @@
-export const icons = {
-  slack: "",
-  teams: "󰊻",
-  outlook: "󰴢",
-  chrome: "",
-  discord: "",
-  vesktop: "",
-  steam: "",
-  blueman: "",
-  bluetooth: "",
-  battery: "󱊣",
-  "input-gaming": "󰊗",
-  default: "󱅫",
+export type AttributeData = {
+  type: string;
+  data: string;
+};
+export type Notification = {
+  'app-name': AttributeData;
+  'app-icon': AttributeData;
+  'summary': AttributeData;
+  'body': AttributeData;
 };
 
-export function toMenuItem(item) {
-  const appName = item["app-name"].data.trim().toLowerCase();
-  const appIcon = item["app-icon"].data.trim().toLowerCase();
+export const icons: Record<string, string> = {
+  slack: '',
+  teams: '󰊻',
+  outlook: '󰴢',
+  chrome: '',
+  discord: '',
+  vesktop: '',
+  steam: '',
+  blueman: '',
+  bluetooth: '',
+  battery: '󱊣',
+  'input-gaming': '󰊗',
+  default: '󱅫',
+};
+
+export function toMenuItem(item: Notification) {
+  const appName = item['app-name'].data.trim().toLowerCase();
+  const appIcon = item['app-icon'].data.trim().toLowerCase();
   const title = item.summary.data.trim();
   const body = item.body.data.trim();
 
-  const icon =
-    (appIcon && icons[appIcon]) ?? (appName && icons[appName]) ?? icons.default;
+  const icon = (appIcon && icons[appIcon]) ?? (appName && icons[appName]) ??
+    icons.default;
 
   let notification = `${icon}  ${title}`;
   if (body) notification += `  -  ${body}`;
