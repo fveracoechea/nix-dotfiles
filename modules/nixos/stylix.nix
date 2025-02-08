50b2189508082400ff9d09dd113ed16309b25f25
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-02-08 11:05:46 -0500

    wip

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -19,7 +19,7 @@ in {
   stylix = {
     enable = true;
     autoEnable = true;
-    image = ../../wallpapers/warrior-ultrawide.png;
+    image = ../../wallpapers/dark-forrest-ultrawide.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
 
