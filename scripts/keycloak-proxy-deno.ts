c3b2d8df174fc836764888f102c1ed0596a01c28
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-02-13 22:13:18 -0500

    update deno proxy

--- scripts/keycloak-proxy-deno.ts
+++ scripts/keycloak-proxy-deno.ts
@@ -1,10 +1,19 @@
 import { parse } from "https://deno.land/std@0.200.0/flags/mod.ts";
 
-Deno.serve(async (request) => {
-  const { target = "https://10.1.80.166:8543", name = "Proxy" } = parse(
+function getIp() {
+  for (const net of Deno.networkInterfaces()) {
+    if (net.family === "IPv4" && !net.address.includes("127.0.0.1")) {
+      return net.address;
+    }
+  }
+}
+
+const { target = "https://10.1.80.166:8543", name = "Proxy", port = 8543 } =
+  parse(
     Deno.args,
   );
 
+Deno.serve({ port }, async (request) => {
   const { pathname, search } = new URL(request.url);
   const url = new URL(pathname + search, target);
 
@@ -12,3 +21,5 @@ Deno.serve(async (request) => {
   console.log(name, url, response.status);
   return response;
 });
+
+console.log(`${name} running on ${getIp()}:${port}`);
