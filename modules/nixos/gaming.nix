2b74e844b87e33f37b1616ae487a070f49e21d19
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-04-01 19:51:55 -0400

    wip

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -55,6 +55,12 @@
           gamemode
         ];
       })
+      (writers.writeBashBin "sunshine-gamescope-session"
+        # bash
+        ''
+          sunshine &> ~/sunshine-output.txt &
+          gamescope -- steam -gamepadui -pipewire-dmabuf -tenfoot -steamos
+        '')
     ];
   };
 
@@ -66,6 +72,43 @@
     serviceConfig = {
       ExecStart = "${pkgs.lact}/bin/lact daemon";
     };
+  };
+
+  xdg.configFile."sunshine/apps.json".text = builtins.toJSON {
+    env = {
+      "PATH" = "$(PATH):$(HOME)/.local/bin";
+    };
+    apps = [
+      {
+        name = "Steam 4K";
+        image-path = "steam.png";
+      }
+    ];
+  };
+
+  services = {
+    sunshine = {
+      enable = true;
+      autoStart = false;
+      capSysAdmin = true;
+      openFirewall = true;
+    };
+
+    displayManager.sessionPackages = [
+      (pkgs.writeTextFile {
+        name = "gamescope-steam.desktop";
+        destination = "/share/wayland-sessions/gamescope-steam.desktop";
+        checkPhase = ''${pkgs.buildPackages.desktop-file-utils}/bin/desktop-file-validate "$target"'';
+        derivationArgs = {passthru.providedSessions = ["gamescope-steam"];};
+        text = ''
+          [Desktop Entry]
+          Name=Gamescope Steam
+          Comment=Launch Gamescope with Steam Big Picture Mode
+          Exec=sunshine-gamescope-session
+          Type=Application
+        '';
+      })
+    ];
   };
 
   networking.interfaces."eno1".wakeOnLan.enable = true;
