c7664e92db89dd7be41ecde1a2e4cdeaf45e6c7e
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-03-31 22:01:28 -0400

    cleanup steam settings

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,29 +1,63 @@
 {pkgs, ...}: {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  hardware = {
+    graphics.enable = true;
+    graphics.enable32Bit = true;
+
+    cpu.amd.updateMicrocode = true;
+    amdgpu.initrd.enable = true;
+
+    # Support for the xbox accessories
+    xone.enable = true;
+
+    # Open-source Vulkan driver from AMD
+    amdgpu.amdvlk = {
+      enable = true;
+      support32Bit.enable = true;
+    };
   };
 
-  hardware.cpu.amd.updateMicrocode = true;
-  hardware.amdgpu.initrd.enable = true;
+  programs = {
+    steam.enable = true;
+    steam.gamescopeSession.enable = true;
+    steam.extraCompatPackages = [pkgs.proton-ge-bin];
 
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
+    gamescope.enable = true;
+    gamescope.capSysNice = true;
+    gamescope.args = [
+      "-w"
+      "3840"
+      "-h"
+      "2160"
+      "-r"
+      "120"
+      "--adaptive-sync" # VRR support
+      "--hdr-enabled"
+      "--rt"
+    ];
+    gamemode.enable = true;
+  };
 
-  environment.systemPackages = with pkgs; [
-    ethtool
-    protonup
-    amdgpu_top
-    lact
-    (lutris.override {
-      extraPkgs = pkgs: [
-        wineWowPackages.waylandFull
-        winetricks
-        gamemode
-      ];
-    })
-  ];
+  environment = {
+    sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    };
+
+    systemPackages = with pkgs; [
+      mesa-demos
+      ethtool
+      protonup
+      amdgpu_top
+      mangohud
+      lact
+      (lutris.override {
+        extraPkgs = pkgs: [
+          wineWowPackages.waylandFull
+          winetricks
+          gamemode
+        ];
+      })
+    ];
+  };
 
   systemd.services.lact = {
     enable = true;
@@ -35,19 +69,33 @@
     };
   };
 
-  services.sunshine = {
-    enable = true;
-    autoStart = true;
-    capSysAdmin = true;
-    openFirewall = true;
+  services = {
+    sunshine = {
+      enable = true;
+      autoStart = true;
+      capSysAdmin = true;
+      openFirewall = true;
+    };
+
+    displayManager.sessionPackages = [
+      (pkgs.writeTextFile {
+        name = "gamescope-steam.desktop";
+        destination = "/share/wayland-sessions/gamescope-steam.desktop";
+        checkPhase = ''${pkgs.buildPackages.desktop-file-utils}/bin/desktop-file-validate "$target"'';
+        derivationArgs = {passthru.providedSessions = ["gamescope-steam"];};
+        text = ''
+          [Desktop Entry]
+          Name=Gamescope Steam
+          Comment=Launch Gamescope with Steam Big Picture Mode
+          Exec=gamescope -e -- steam -pipewire-dmabuf -tenfoot
+          Type=Application
+        '';
+      })
+    ];
   };
 
   networking.interfaces."eno1".wakeOnLan.enable = true;
 
   # Enable openrgb
   services.hardware.openrgb.enable = true;
-
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-  };
 }
