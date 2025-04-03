41a2e405f1bab9097319799ced2c1c21b18fce74
Francisco Veracoechea <francisco.veracoechea@magnitglobal.com>
2025-04-03 14:20:04 -0400

    WIP proxy changes

--- packages/scripts/keycloak-proxy.js
+++ packages/scripts/keycloak-proxy.js
@@ -1,9 +1,9 @@
 const http = require("node:http");
 const https = require("node:https");
+const { URL } = require('node:url');
 const os = require("node:os");
 
-const TARGET_HOST = "https://pmwand4-iam.prounlimited.com";
-const TARGET_PORT = 3330;
+const PORT = 3330;
 
 /**
  * Get the local IP address of the machine.
@@ -21,32 +21,37 @@ function getLocalIp() {
   return "Unknown";
 }
 
+
+const TARGET_URL = 'https://pmwand4-iam.prounlimited.com';
+
 const server = http.createServer((req, res) => {
-  console.log("KEYCLOAK PROXY ", req.url);
+    const targetUrl = new URL(TARGET_URL + req.url);
+    
+    
+    const options = {
+        hostname: targetUrl.hostname,
+        path: targetUrl.pathname + targetUrl.search,
+        method: req.method,
+        headers: req.headers,
+        rejectUnauthorized: false,  // Bypass SSL verification (use cautiously)
+        ciphers: 'DEFAULT:@SECLEVEL=0', // Allow weaker SSL ciphers if needed
+    };    
 
-  const options = {
-    hostname: TARGET_HOST,
-    path: req.url || "/",
-    method: req.method,
-    headers: req.headers,
-    rejectUnauthorized: false, // Ignore SSL certificate issues
-  };
-
-  const proxyReq = https.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode ?? 500, proxyRes.headers);
-    proxyRes.pipe(res);
-  });
-
-  proxyReq.on("error", (err) => {
-    res.writeHead(500);
-    res.end(`Error: ${err.message}`);
-  });
-
-  req.pipe(proxyReq);
+    const proxyReq = https.request(options, (proxyRes) => {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res);
+    });
+    
+    proxyReq.on('error', (err) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Proxy error: ' + err.message);
+    });
+    
+    req.pipe(proxyReq);
 });
 
 const localIp = getLocalIp();
 
-server.listen(TARGET_PORT, () => {
-  console.log(`Proxy server running on http://${localIp}:${TARGET_PORT}`);
+server.listen(PORT, () => {
+  console.log(`Keycloak Proxy server running on http://${localIp}:${PORT}`);
 });
