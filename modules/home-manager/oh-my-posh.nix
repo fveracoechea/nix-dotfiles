dae31a5b62b7c5c9f93386d460daa1558bf59500
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-06-21 09:47:44 -0400

    oh-my-posh update

--- modules/home-manager/oh-my-posh.nix
+++ modules/home-manager/oh-my-posh.nix
@@ -1,117 +1,74 @@
 {pkgs, ...}: let
   catppuccin = import ../../utils/catppuccin.nix;
 in {
-  # Oh-My-Posh theme and setup
+  # oh-my-posh theme and setup
   home.packages = [pkgs.oh-my-posh];
 
   xdg.configFile."zsh/oh-my-posh/catppuccin.json".text = builtins.toJSON {
     version = 3;
     final_space = true;
-
-    transient_prompt = {
-      background = "transparent";
-      template = "\n<${catppuccin.rosewater}>  {{ .PWD }}</> ❯ ";
-      foreground_templates = [
-        "{{if gt .Code 0}}${catppuccin.red}{{end}}"
-        "{{if eq .Code 0}}${catppuccin.mauve}{end}}"
-      ];
-    };
+    enable_cursor_positioning = true;
 
     secondary_prompt = {
-      foreground = catppuccin.mauve;
+      foreground = catppuccin.text;
       background = "transparent";
       template = "❯❯ ";
     };
 
-    blocks = [
+    blocks = let
+      divider = {
+        type = "text";
+        style = "plain";
+        foreground = catppuccin.text;
+        background = "transparent";
+        template = "  ";
+      };
+    in [
       {
         type = "prompt";
         alignment = "left";
+        newline = true;
         segments = [
           {
-            type = "text";
-            style = "plain";
-            template = "\n";
-          }
-          {
             type = "os";
-            style = "diamond";
-            powerline_symbol = "";
-            foreground = catppuccin.crust;
-            background = catppuccin.blue;
-            template = " {{.Icon}} ";
+            style = "plain";
+            foreground = catppuccin.text;
+            background = "transparent";
+            template = "\n{{ .Icon }} ";
           }
           {
             type = "session";
-            style = "diamond";
-            powerline_symbol = "";
-            foreground = catppuccin.crust;
-            background = catppuccin.blue;
-            template = "<b>{{ .UserName }}</b>";
+            style = "plain";
+            foreground = catppuccin.blue;
+            background = "transparent";
+            template = " {{ .UserName }}{{ if .SSHSession }}    {{ .HostName }}{{ end }}";
           }
+          divider
           {
             type = "path";
-            style = "powerline";
-            foreground = catppuccin.blue;
-            background = catppuccin.surface0;
-            powerline_symbol = "";
+            style = "plain";
+            foreground = catppuccin.pink;
+            background = "transparent";
+            template = "{{ .Path }}";
             properties = {
               home_icon = "~";
               style = "agnoster_full";
             };
-            template = "  {{ .Path }} ";
           }
-          {
-            type = "git";
-            foreground = catppuccin.rosewater;
-            style = "plain";
-            properties = {
-              branch_icon = " ";
-              cherry_pick_icon = " ";
-              commit_icon = " ";
-              fetch_status = true;
-              fetch_upstream_icon = false;
-              merge_icon = " ";
-              no_commits_icon = " ";
-              rebase_icon = " ";
-              revert_icon = " ";
-              tag_icon = " ";
-            };
-            templates = [
-              " {{ .HEAD }}"
-              "{{if .BranchStatus}}  {{ .BranchStatus }}{{ end }}"
-              "{{if .Working.Changed}}  {{ .Working.String }}{{ end }}"
-            ];
-          }
-        ];
-      }
-
-      {
-        type = "prompt";
-        alignment = "right";
-        overflow = "hide";
-        segments = [
+          divider
           {
             type = "executiontime";
             style = "plain";
-            foreground = catppuccin.rosewater;
-            template = " {{ .FormattedMs }}  ";
+            foreground = catppuccin.lavender;
+            background = "transparent";
+            template = " {{ .FormattedMs }}";
             properties = {
               style = "austin";
               always_enabled = true;
             };
           }
-          {
-            type = "node";
-            style = "diamond";
-            leading_diamond = "";
-            foreground = catppuccin.green;
-            background = catppuccin.surface0;
-            template = " {{ .Full }}  ";
-          }
         ];
       }
-
       {
         type = "prompt";
         alignment = "left";
@@ -120,11 +77,11 @@ in {
           {
             type = "text";
             style = "plain";
-            template = "❯";
+            template = "❯ ";
             background = "transparent";
             foreground_templates = [
               "{{if gt .Code 0}}${catppuccin.red}{{end}}"
-              "{{if eq .Code 0}}${catppuccin.blue}{{end}}"
+              "{{if eq .Code 0}}${catppuccin.text}{{end}}"
             ];
           }
         ];
--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -12,12 +12,10 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
-  programs.ssh.startAgent = true;
-
   programs.hyprland = {
     enable = true;
     withUWSM = true;
-    # xwayland.enable = true;
+    xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
