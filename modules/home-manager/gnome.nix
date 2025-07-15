5bcbac10e599491b0599bacab9445ccb51df9a2e
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-07-14 22:00:30 -0400

    cleanup gnome

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -15,19 +15,11 @@
   };
 
   home.packages = with pkgs; [
-    dconf-editor
-    gnome-tweaks
     gnome-system-monitor
     gnome-calculator
     gnome-control-center
     gnome-notes
     gnome-firmware
     gnome-monitor-config
-    gnomeExtensions.freon
-    gnomeExtensions.gamemode-shell-extension
-    gnomeExtensions.tiling-shell
-    gnomeExtensions.system-monitor
-    gnomeExtensions.workspace-indicator
-    gnomeExtensions.auto-move-windows
   ];
 }
