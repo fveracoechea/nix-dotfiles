36f28a30e9739d90771b34420de33eef45aa9ff5
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-07-14 22:48:28 -0400

    fix dev-manager pkgs

--- packages/dev-manager-desktop.nix
+++ packages/dev-manager-desktop.nix
@@ -19,22 +19,23 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "dev-manager-desktop";
-  # TODO: v1.99.13 didn't build check if a later version fixes it
-  version = "1.99.10";
+  version = "1.99.16";
   useFetchCargoVendor = true;
-  cargoHash = "sha256-AhQ3bQ4wCP3qFy1Yk8hqSrBCU4QXEWED7BPAzDZpsGA=";
+  cargoHash = "sha256-a6H7MEHNkOOfw1VdvYQ/ZN8myVcBIk8fdm6LUHUYOAg=";
 
   src = fetchFromGitHub {
     repo = pname;
     rev = "v${version}";
     owner = "webosbrew";
-    hash = "sha256-IrwWfEJzQxfp8BKZuqOk7aY28Ozuuo4TCp/p0b1saS4=";
+    hash = "sha256-mWqPEAQW59HRDoV3JEosAxe3IrFgSihWB0Joz1rpdh8=";
   };
+
+  doCheck = false;
 
   npmDeps = fetchNpmDeps {
     name = "${pname}-npm-deps-${version}";
     inherit src;
-    hash = "sha256-VnIyUr6gWQXV6/gRUWfui2EXUBI9Gk3CVJgJ9jfRI5M=";
+    hash = "sha256-YYm8nZ5eKoy1kx28oA2buocQB3bZ6IGWV48pYDJZR4g=";
   };
 
   nativeBuildInputs = [
@@ -58,6 +59,5 @@ rustPlatform.buildRustPackage rec {
       webkitgtk_4_1
       gtk4
       gtk3
-      webkitgtk_4_1
     ];
 }
