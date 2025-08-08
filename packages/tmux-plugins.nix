15195719a4848f434965674889601309e84d7ea6
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-08-08 11:43:41 -0400

    WIP

--- packages/tmux-plugins.nix
+++ packages/tmux-plugins.nix
@@ -3,14 +3,14 @@
   tmuxPlugins,
   ...
 }: {
-  catppuccin = tmuxPlugins.mkTmuxPlugin rec {
+  catppuccin = tmuxPlugins.mkTmuxPlugin {
     pluginName = "catppuccin";
-    version = "2.1.3";
+    version = "Commits on Aug 6, 2025";
     src = fetchFromGitHub {
       owner = "catppuccin";
       repo = "tmux";
-      rev = "v${version}";
-      hash = "sha256-Is0CQ1ZJMXIwpDjrI5MDNHJtq+R3jlNcd9NXQESUe2w=";
+      rev = "9d21d7ccd50df82bd732be2850ce2798e78b6391";
+      hash = "sha256-Xk3LogY3B2bZbUWyTHa2nap4Rn0bU2QgYlHlkngMPsk=";
     };
     postInstall = ''
       sed -i -e 's|''${PLUGIN_DIR}/catppuccin-selected-theme.tmuxtheme|''${TMUX_TMPDIR}/catppuccin-selected-theme.tmuxtheme|g' $target/catppuccin.tmux
