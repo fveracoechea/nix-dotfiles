bdfbb1042b590a9a1832528db1b932de2e6baa7b
Francisco Veracoechea <francisco.veracoechea@magnitglobal.com>
2026-02-11 15:36:16 -0500

    update darwin settings

--- modules/darwin/system-defaults.nix
+++ modules/darwin/system-defaults.nix
@@ -3,12 +3,13 @@
   system.defaults = {
     finder.AppleShowAllExtensions = true;
     NSGlobalDomain.AppleShowAllFiles = true;
+    universalaccess.mouseDriverCursorSize = 1.5;
     dock = {
-      autohide = true;
+      autohide = false;
       autohide-delay = 0.0;
       mru-spaces = false;
       largesize = 65;
-      tilesize = 35;
+      tilesize = 50;
       magnification = true;
       mineffect = "genie";
       orientation = "bottom";
