0578e8886096bdee2bc9609d68af5f8d14716ef8
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-06-08 16:05:41 -0400

    nix-darwin patch

--- flake.nix
+++ flake.nix
@@ -26,7 +26,9 @@
     spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     # Nix Darwin
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    # nix-darwin.url = "github:LnL7/nix-darwin";
+    # INFO: waiting for fix to be merged/reviewed https://github.com/nix-darwin/nix-darwin/pull/1789
+    nix-darwin.url = "github:stepbrobd/nix-darwin/bundle";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # Shell utilities
